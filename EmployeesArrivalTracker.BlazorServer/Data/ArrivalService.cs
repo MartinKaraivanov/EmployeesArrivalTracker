85c88350f7902f98af85b66ce4d02eb6949375f0
MartinKaraivanov <87902145+MartinKaraivanov@users.noreply.github.com>
2022-08-09 11:45:49 +0300

    Expanded table

--- EmployeesArrivalTracker.BlazorServer/Data/ArrivalService.cs
+++ EmployeesArrivalTracker.BlazorServer/Data/ArrivalService.cs
@@ -1,31 +1,30 @@
 ﻿using EmployeesArrivalTracker.BlazorServer.Data;
 using EmployeesArrivalTracker.BlazorServer.Entities;
 
-namespace EmployeesArrivalTracker.BlazorServer.Data
+namespace EmployeesArrivalTracker.BlazorServer.Data;
+
+public class ArrivalService
 {
-    public class ArrivalService
+    private readonly EmployeesArrivalDbContext _dbContext;
+
+    public ArrivalService(EmployeesArrivalDbContext dbContext)
     {
-        private readonly EmployeesArrivalDbContext _dbContext;
-
-        public ArrivalService(EmployeesArrivalDbContext dbContext)
-        {
-            _dbContext = dbContext;
-        }
-
-        public Task<Arrival[]> GetArrivalsAsync()
-        {
-            return Task.FromResult(_dbContext.Arrivals.OrderByDescending(x => x.ArrivalDatetime).ToArray());
-        }
-
-        public Arrival[] GetById(int employeeId)
-        {
-            return _dbContext.Arrivals.Where(x => x.EmployeeId == employeeId).ToArray();
-        }
-
-        public Arrival[] GetByArrivalDatetime(DateTime arrivalDatetimeFrom, DateTime arrivalDatetimeTo)
-        {
-            return _dbContext.Arrivals.Where(x => x.ArrivalDatetime >= arrivalDatetimeFrom && x.ArrivalDatetime <= arrivalDatetimeTo).ToArray();
-        }
-        
+        _dbContext = dbContext;
     }
+
+    public Task<ArrivalViewModel[]> GetArrivalsAsync()
+    {
+        return Task.FromResult(_dbContext.Arrivals.Select(a => new ArrivalViewModel() { EmployeeId = a.EmployeeId, FirstName = a.Employee.FirstName, LastName = a.Employee.LastName, Age = a.Employee.Age, ArrivalDatetime = a.ArrivalDatetime }).OrderByDescending(a => a.ArrivalDatetime).ToArray());
+    }
+
+    public ArrivalViewModel[] GetById(int employeeId)
+    {
+        return _dbContext.Arrivals.Select(a => new ArrivalViewModel() { EmployeeId = a.EmployeeId, FirstName = a.Employee.FirstName, LastName = a.Employee.LastName, Age = a.Employee.Age, ArrivalDatetime = a.ArrivalDatetime }).Where(a => a.EmployeeId == employeeId).ToArray();
+    }
+
+    public ArrivalViewModel[] GetByArrivalDatetime(DateTime arrivalDatetimeFrom, DateTime arrivalDatetimeTo)
+    {
+        return _dbContext.Arrivals.Select(a => new ArrivalViewModel() { EmployeeId = a.EmployeeId, FirstName = a.Employee.FirstName, LastName = a.Employee.LastName, Age = a.Employee.Age, ArrivalDatetime = a.ArrivalDatetime }).Where(x => x.ArrivalDatetime >= arrivalDatetimeFrom && x.ArrivalDatetime <= arrivalDatetimeTo).ToArray();
+    }
+    
 }
---
+++ EmployeesArrivalTracker.BlazorServer/Entities/ArrivalViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace EmployeesArrivalTracker.BlazorServer.Entities
+{
+    public class ArrivalViewModel
+    {
+        public int EmployeeId { get; set; }
+
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public int Age { get; set; }
+
+        public DateTime ArrivalDatetime { get; set; }
+    }
+}
