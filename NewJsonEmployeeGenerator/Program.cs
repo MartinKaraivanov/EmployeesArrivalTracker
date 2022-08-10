dae0b7b5f753408231b608385c0cd96d1deed11c
MartinKaraivanov <87902145+MartinKaraivanov@users.noreply.github.com>
2022-08-10 21:52:25 +0300

    New implementation of JsonEmployeeGenerator

--- NewJsonEmployeeGenerator/Program.cs
+++ NewJsonEmployeeGenerator/Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+
+namespace NewJsonEmployeeGenerator;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public int? ManagerId { get; set; }
+    public int Age { get; set; }
+    public List<string>? Teams { get; set; }
+    public string? Role { get; set; }
+    public string? Email { get; set; }
+    public string? SurName { get; set; }
+    public string? Name { get; set; }
+}
+
+public class Program
+{
+    private static readonly string[] roles = new string[] { "Junior Developer", "Semi Senior Developer", "Senior Developer", "Principal", "Team Leader" };
+    private static readonly string[] teams = new string[] { "Platform", "Sales", "Billing", "Mirage" };
+
+    private static Random generator = new Random();
+
+    static Employee getEmployee(string line, int lineCounter)
+    {
+
+        int? managerId = null;
+        List<string> employeeTeams = new List<string>();
+        string? role = "Manager";
+
+        if (lineCounter >= 11)
+        {
+            managerId = generator.Next(11);
+            int count = generator.Next(1, 4);
+            for (int j = 0; j < count; j++)
+            {
+                employeeTeams.Add(teams[generator.Next(4)]);
+            }
+            role = roles[generator.Next(4)];
+        }
+
+        Employee employee = new Employee
+        {
+            Id = lineCounter,
+            ManagerId = managerId,
+            Age = generator.Next(18, 66),
+            Teams = employeeTeams,
+            Role = role,
+            Email = line.Split('\t')[2],
+            SurName = line.Split('\t')[1],
+            Name = line.Split('\t')[0]
+        };
+
+        return employee;
+    }
+    public static void Main()
+    {
+        string readFileName = @"C:\Prj\Fourth\EmployeesArrivalTracker\NewJsonEmployeeGenerator\employees.txt";
+        string jsonFileName = @"C:\Prj\Fourth\EmployeesArrivalTracker\NewJsonEmployeeGenerator\employees.json";
+
+        var query = File.ReadLines(readFileName).Select(getEmployee);
+
+        using FileStream createStream = File.Create(jsonFileName);
+        JsonSerializer.Serialize(createStream, query, new JsonSerializerOptions { WriteIndented = true });
+    }
+}
